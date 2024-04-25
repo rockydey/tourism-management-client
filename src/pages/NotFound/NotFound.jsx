2bde3f2012a0f9dbffd8567113f34670f95c07c9
Rocky Dey <coderrocky25@gmail.com>
2024-04-25 21:18:03 +0600

    website favicon and not found page complete

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+import NotFoundImg from "../../assets/404.jpg";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+
 const NotFound = () => {
-  return <div></div>;
+  return (
+    <div className='my-20'>
+      <div className='px-5 md:px-10 lg:px-0 lg:w-5/12 mx-auto'>
+        <Link
+          to='/'
+          className='flex items-center gap-3 justify-center font-gothic text-2xl font-bold text-color3F'>
+          <FaArrowAltCircleLeft /> <span>Back to Home</span>
+        </Link>
+        <img className='w-full' src={NotFoundImg} alt='' />
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
